42ea1619ebdda8877e3f9a1c36fd051dd73ac22c
Amr khaled <115163788+Amrdrago@users.noreply.github.com>
2024-05-07 22:55:28 +0300

    Update Data_Preprocessing.py

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -9,8 +9,8 @@ missing_values = data.isnull().sum()
 print("Missing values:\n", missing_values)
 
 # Step 2: Encode categorical variables
-categorical_cols = ['gender']  # Replace with actual categorical columns
-data_encoded = pd.get_dummies(data, columns=categorical_cols)
+categorical_cols = ['gender', 'smoking_history']  # Replace with actual categorical columns
+data_encoded = pd.get_dummies(data, columns=categorical_cols, dtype=int)
 
 # Step 3: Normalize or scale numerical features
 numerical_cols = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level']  # Replace with actual numerical columns
@@ -21,11 +21,8 @@ print(data.head(10).to_string())
 # Replace the original numerical columns with scaled ones
 data_encoded[numerical_cols] = data_scaled
 
-# Replace gender_Male with 1 and gender_Female with 0
-data_encoded['gender_Female'] = (data_encoded['gender_Female'] == 1).astype(int)
-data_encoded['gender_Male'] = (data_encoded['gender_Male'] == 1).astype(int)
-
 data_encoded.drop('gender_Other', axis=1, inplace=True)
 
 # Save preprocessed data to a new CSV file
 data_encoded.to_csv("preprocessed_diabetes_dataset.csv", index=False)
+
